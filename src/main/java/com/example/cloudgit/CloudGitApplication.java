da622792c4741966f2e9ce783b1278b7bf617496
”ayue0926“git config --global user.email“2537319731@qq.com <2537319731@qq.com>
2019-05-05 14:29:22 +0800

    第一次提交

---
+++ src/main/java/com/example/cloudgit/CloudGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGitApplication.class, args);
+	}
+
+}
